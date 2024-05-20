4cf029cd203a328c32b242f4bb8b1c79fda60802
thednp <thednp@users.noreply.github.com>
2024-05-20 20:03:49 +0300

    fix

--- demo/vite.config.docs.ts
+++ demo/vite.config.docs.ts
@@ -15,7 +15,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '~': path.resolve(__dirname, '..', 'src'),
-      // routes: `${path.resolve(__dirname, "./src/routes/")}`,
       '@': path.resolve(__dirname, 'assets'),
     },
   },
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import generateIcons from '../generate';
 
-if (typeof generateIcons === 'function') {
-  generateIcons();
-}
+// import generateIcons from '../generate';
+// if (typeof generateIcons === 'function') {
+//   generateIcons();
+// }
 
 export default defineConfig({
   plugins: [],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
-      // routes: `${path.resolve(__dirname, "./src/routes/")}`,
       '@': path.resolve(__dirname, 'assets'),
     },
   },
